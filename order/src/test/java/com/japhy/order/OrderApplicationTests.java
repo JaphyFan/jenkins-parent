46f2b0fe3a42ad76700679f509283e377250178b
Japhy <920984324@qq.com>
2020-04-06 16:21:54 +0800

    push test code

---
+++ order/src/test/java/com/japhy/order/OrderApplicationTests.java
@@ -0,0 +1,14 @@
+package com.japhy.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("hello world");
+    }
+
+}
---
+++ order/src/test/java/com/japhy/order/domain/order/service/impl/OrderServiceImplTest.java
@@ -0,0 +1,41 @@
+package com.japhy.order.domain.order.service.impl;
+
+import com.japhy.order.domain.order.model.Order;
+import com.japhy.order.domain.order.service.OrderService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * test.
+ *
+ * @author Japhy
+ * @since 2020/4/5 09:30
+ */
+@SpringBootTest
+class OrderServiceImplTest {
+
+    @Autowired
+    private OrderService orderService;
+
+    @Test
+    void getById() {
+        Order order = orderService.getById(2L);
+        Assertions.assertThat(order.getOrderNo()).isEqualTo("0002");
+
+    }
+
+    @Test
+    void queryByName() {
+        // not test
+    }
+
+    /**
+     * just for test report purpose
+     */
+    @Test
+    void report() {
+        Assertions.assertThat(1).isEqualTo(1);
+    }
+}
